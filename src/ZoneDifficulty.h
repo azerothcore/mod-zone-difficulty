cd436d8ae60e9d05c91056ed7c2a899674c10c2c
55Honey <71938210+55Honey@users.noreply.github.com>
2022-11-20 13:58:54 +0100

    adjust const type

--- src/ZoneDifficulty.h
+++ src/ZoneDifficulty.h
@@ -16,7 +16,7 @@ struct ZoneDifficultyData
 };
 
 int DUEL_INDEX = 0x7FFFFFFF;
-int DUEL_AREA = 2401;       // Forbidding Sea (Arathi Highlands)
+uint32 DUEL_AREA = 2401;       // Forbidding Sea (Arathi Highlands)
 
 class ZoneDifficulty
 {
