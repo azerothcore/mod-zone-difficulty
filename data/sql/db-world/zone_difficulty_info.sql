c63b0bba2028d058113b3b87dda2386e01a04390
55Honey <71938210+55Honey@users.noreply.github.com>
2022-11-22 23:01:25 +0100

    sql fix

--- data/sql/db-world/zone_difficulty_info.sql
+++ data/sql/db-world/zone_difficulty_info.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `zone_difficulty_info`;
 CREATE TABLE `zone_difficulty_info` (
     `MapID` INT NOT NULL DEFAULT 0,
-    `Phase` INT NOT NULL DEFAULT 0,
+    `PhaseMask` INT NOT NULL DEFAULT 0,
     `HealingNerfValue` FLOAT NOT NULL DEFAULT 1,
 	`AbsorbNerfValue` FLOAT NOT NULL DEFAULT 1,
 	`MeleeDmgBuffValue` FLOAT NOT NULL DEFAULT 1,
--- data/sql/db-world/zone_difficulty_info_content.sql
+++ data/sql/db-world/zone_difficulty_info_content.sql
@@ -1,4 +1,4 @@
 DELETE FROM `zone_difficulty_info`;
-INSERT INTO `zone_difficulty_info` (`MapId`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
+INSERT INTO `zone_difficulty_info` (`MapId`, `PhaseMask`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
 (531, 0, '0.10', '0.10', '1.40', '1.40', 1, 'AQ40 Healing 90% / Absorb 90% Nerf / 40% physical & spell damage buff'),
 (2147483647, 0, '0.10', '0.10', '0.5', '0.5', 1, 'Duel Healing 90% / Absorb 90% Nerf / 50% physical & spell damage nerf');
