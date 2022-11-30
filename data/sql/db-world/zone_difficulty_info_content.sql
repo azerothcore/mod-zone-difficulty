99c4d5eccba2d1d68e78bebf6756624c386543f5
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-11-29 23:47:09 -0300

    feat: Add Battleground nerfs

--- data/sql/db-world/zone_difficulty_info_content.sql
+++ data/sql/db-world/zone_difficulty_info_content.sql
@@ -3,6 +3,11 @@ INSERT INTO `zone_difficulty_info` (`MapId`, `PhaseMask`, `HealingNerfValue`, `A
 (531, 0, '0.50', '0.50', '1.50', '1.30', 1, 'AQ40 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
 (509, 0, '0.50', '0.50', '1.60', '1.30', 1, 'AQ20 Healing 50% / Absorb 50% Nerf / 60% physical & 30% spell damage buff'),
 (309, 0, '0.50', '0.50', '1.50', '1.10', 1, 'ZG20 Healing 50% / Absorb 50% Nerf / 50% physical & 10% spell damage buff'),
-(469, 0, '0.50', '0.50', '1.50', '1.50', 1, 'BWL  Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
-(249, 0, '0.50', '0.50', '1.40', '1.40', 1, 'ONY  Healing 50% / Absorb 50% Nerf / 40% physical & 40% spell damage buff'),
-(409, 0, '0.50', '0.50', '1.50', '1.50', 1, 'MC   Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff');
+(469, 0, '0.50', '0.50', '1.50', '1.50', 1, 'BWL Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
+(249, 0, '0.50', '0.50', '1.40', '1.40', 1, 'ONY Healing 50% / Absorb 50% Nerf / 40% physical & 40% spell damage buff'),
+(409, 0, '0.50', '0.50', '1.50', '1.50', 1, 'MC Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
+-- Battlegrounds
+(30, 0, '0.20', '0.20', '0.30', '0.30', 1, 'AV Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
+(489, 0, '0.20', '0.20', '0.30', '0.30', 1, 'WSG Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
+(529, 0, '0.20', '0.20', '0.30', '0.30', 1, 'AB Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
+(556, 0, '0.20', '0.20', '0.30', '0.30', 1, 'AV Healing 20% / Absorb 20% / 30% physical & spell damage nerf');
