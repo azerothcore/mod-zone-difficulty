f18dccccba6fb2d93bcb878b4c01f10a5367689c
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-11-29 23:40:19 -0300

    Update zone_difficulty_info_content.sql

--- data/sql/db-world/zone_difficulty_info_content.sql
+++ data/sql/db-world/zone_difficulty_info_content.sql
@@ -1,4 +1,8 @@
 DELETE FROM `zone_difficulty_info`;
-INSERT INTO `zone_difficulty_info` (`MapId`, `PhaseMask`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
-(531, 0, '0.10', '0.10', '1.40', '1.40', 1, 'AQ40 Healing 90% / Absorb 90% Nerf / 40% physical & spell damage buff'),
-(2147483647, 0, '0.10', '0.10', '0.5', '0.5', 1, 'Duel Healing 90% / Absorb 90% Nerf / 50% physical & spell damage nerf');
+INSERT INTO `zone_difficulty_info` (`MapId`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
+(531, '0.50', '0.50', '1.50', '1.30', 1, 'AQ40 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
+(509, '0.50', '0.50', '1.60', '1.30', 1, 'AQ20 Healing 50% / Absorb 50% Nerf / 60% physical & 30% spell damage buff'),
+(309, '0.50', '0.50', '1.50', '1.10', 1, 'ZG20 Healing 50% / Absorb 50% Nerf / 50% physical & 10% spell damage buff'),
+(469, '0.50', '0.50', '1.50', '1.50', 1, 'BWL  Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
+(249, '0.50', '0.50', '1.40', '1.40', 1, 'ONY  Healing 50% / Absorb 50% Nerf / 40% physical & 40% spell damage buff'),
+(409, '0.50', '0.50', '1.50', '1.50', 1, 'MC   Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff');
