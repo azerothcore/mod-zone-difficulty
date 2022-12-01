a6d49170c6f442c42bf5a8cb9b27c077db2a594a
55Honey <71938210+55Honey@users.noreply.github.com>
2022-12-01 01:00:21 +0100

    chore(db): disallowed buffs/duel tuning (#11)

--- data/sql/db-world/zone_difficulty_disallowed_buffs.sql
+++ data/sql/db-world/zone_difficulty_disallowed_buffs.sql
@@ -9,4 +9,10 @@ CREATE TABLE `zone_difficulty_disallowed_buffs` (
 
 DELETE FROM `zone_difficulty_disallowed_buffs`;
 INSERT INTO `zone_difficulty_disallowed_buffs` (`MapID`, `DisallowedBuffs`, `Enabled`, `Comment`) VALUES
-(531, '22888 24425', 1, 'Forbid in AQ40: Rallying Cry of the Dragonslayer, Spirit of Zandalar');
+# (229, '15366 16609 22888 24425 22817 22818 22820 15123', 1, 'Forbid in Blackrock Spire: Songflower Serenade, Warchiefs Blessing, Rallying Cry of the Dragonslayer, Spirit of Zandalar, Fengus\' Ferocity\', Mol\'dar\'s Moxie, Slip\'kik\'s Savvy, Resist Fire from Scarshield Spellbinder'),
+(409, '15366 16609 22888 24425 22817 22818 22820 15123', 1, 'Forbid in Molten Core: Songflower Serenade, Warchiefs Blessing, Rallying Cry of the Dragonslayer, Spirit of Zandalar, Fengus\' Ferocity\', Mol\'dar\'s Moxie, Slip\'kik\'s Savvy, Resist Fire from Scarshield Spellbinder'),
+(249, '15366 16609 22888 24425 22817 22818 22820 15123', 1, 'Forbid in Onyxia\'s Lair: Songflower Serenade, Warchiefs Blessing, Rallying Cry of the Dragonslayer, Spirit of Zandalar, Fengus\' Ferocity\', Mol\'dar\'s Moxie, Slip\'kik\'s Savvy, Resist Fire from Scarshield Spellbinder'),
+(469, '15366 16609 22888 24425 22817 22818 22820 15123', 1, 'Forbid in Blackwing Lair: Songflower Serenade, Warchiefs Blessing, Rallying Cry of the Dragonslayer, Spirit of Zandalar, Fengus\' Ferocity\', Mol\'dar\'s Moxie, Slip\'kik\'s Savvy, Resist Fire from Scarshield Spellbinder'),
+(509, '15366 16609 22888 24425 22817 22818 22820 15123', 1, 'Forbid in Ruins of Ahn\'Qiraj: Songflower Serenade, Warchiefs Blessing, Rallying Cry of the Dragonslayer, Spirit of Zandalar, Fengus\' Ferocity\', Mol\'dar\'s Moxie, Slip\'kik\'s Savvy, Resist Fire from Scarshield Spellbinder'),
+(309, '15366 16609 22888 24425 22817 22818 22820 15123', 1, 'Forbid in Zul Gurub: Songflower Serenade, Warchiefs Blessing, Rallying Cry of the Dragonslayer, Spirit of Zandalar, Fengus\' Ferocity\', Mol\'dar\'s Moxie, Slip\'kik\'s Savvy, Resist Fire from Scarshield Spellbinder'),
+(531, '15366 16609 22888 24425 22817 22818 22820 15123', 1, 'Forbid in Temple of Ahn\'Qiraj: Songflower Serenade, Warchiefs Blessing, Rallying Cry of the Dragonslayer, Spirit of Zandalar, Fengus\' Ferocity\', Mol\'dar\'s Moxie, Slip\'kik\'s Savvy, Resist Fire from Scarshield Spellbinder');
--- data/sql/db-world/zone_difficulty_info_content.sql
+++ data/sql/db-world/zone_difficulty_info_content.sql
@@ -1,5 +1,6 @@
 DELETE FROM `zone_difficulty_info`;
 INSERT INTO `zone_difficulty_info` (`MapId`, `PhaseMask`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
+-- Raids
 (531, 0, '0.50', '0.50', '1.50', '1.30', 1, 'AQ40 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
 (509, 0, '0.50', '0.50', '1.60', '1.30', 1, 'AQ20 Healing 50% / Absorb 50% Nerf / 60% physical & 30% spell damage buff'),
 (309, 0, '0.50', '0.50', '1.50', '1.10', 1, 'ZG20 Healing 50% / Absorb 50% Nerf / 50% physical & 10% spell damage buff'),
@@ -10,4 +11,6 @@ INSERT INTO `zone_difficulty_info` (`MapId`, `PhaseMask`, `HealingNerfValue`, `A
 (30, 0, '0.20', '0.20', '0.30', '0.30', 1, 'AV Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
 (489, 0, '0.20', '0.20', '0.30', '0.30', 1, 'WSG Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
 (529, 0, '0.20', '0.20', '0.30', '0.30', 1, 'AB Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
-(556, 0, '0.20', '0.20', '0.30', '0.30', 1, 'AV Healing 20% / Absorb 20% / 30% physical & spell damage nerf');
+(556, 0, '0.20', '0.20', '0.30', '0.30', 1, 'AV Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
+-- Duels in Forbidding Sea (Wetlands)
+(2147483647, 0, '0.80', '0.80', '0.70', '0.70', 1, 'Zone 2402 Duel Healing 20% / Absorb 20% Nerf / 30% physical & spell damage nerf');
