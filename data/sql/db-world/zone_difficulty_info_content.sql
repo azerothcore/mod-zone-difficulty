99d771673a3dc647f82053659361984934aab5cd
55Honey <71938210+55Honey@users.noreply.github.com>
2022-12-22 21:50:57 +0100

    remove MC from nerfed zones

--- data/sql/db-world/zone_difficulty_info_content.sql
+++ data/sql/db-world/zone_difficulty_info_content.sql
@@ -6,7 +6,7 @@ INSERT INTO `zone_difficulty_info` (`MapId`, `PhaseMask`, `HealingNerfValue`, `A
 (309, 0, '0.50', '0.50', '1.50', '1.10', 1, 'ZG20 Healing 50% / Absorb 50% Nerf / 50% physical & 10% spell damage buff'),
 (469, 0, '0.50', '0.50', '1.50', '1.50', 1, 'BWL Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
 (249, 0, '0.50', '0.50', '1.40', '1.40', 1, 'ONY Healing 50% / Absorb 50% Nerf / 40% physical & 40% spell damage buff'),
-(409, 0, '0.50', '0.50', '1.50', '1.50', 1, 'MC Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
+-- (409, 0, '0.50', '0.50', '1.50', '1.50', 1, 'MC Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
 -- Battlegrounds
 (30, 0, '0.80', '0.80', '0.70', '0.70', 1, 'AV Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
 (489, 0, '0.80', '0.80', '0.70', '0.70', 1, 'WSG Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
