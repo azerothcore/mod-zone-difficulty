c89b685bfd82af886c5d0c0d67b0f9b409d06151
55Honey <71938210+55Honey@users.noreply.github.com>
2023-01-15 19:18:36 +0100

    tbc nerf data

--- data/sql/db-world/zone_difficulty_info_content.sql
+++ data/sql/db-world/zone_difficulty_info_content.sql
@@ -1,12 +1,15 @@
 DELETE FROM `zone_difficulty_info`;
 INSERT INTO `zone_difficulty_info` (`MapId`, `PhaseMask`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
--- Raids
+-- Vanilla Raids
 (531, 0, '0.50', '0.50', '1.50', '1.30', 1, 'AQ40 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
 (509, 0, '0.50', '0.50', '1.60', '1.30', 1, 'AQ20 Healing 50% / Absorb 50% Nerf / 60% physical & 30% spell damage buff'),
 (309, 0, '0.50', '0.50', '1.50', '1.10', 1, 'ZG20 Healing 50% / Absorb 50% Nerf / 50% physical & 10% spell damage buff'),
 (469, 0, '0.50', '0.50', '1.50', '1.50', 1, 'BWL Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
 (249, 0, '0.50', '0.50', '1.40', '1.40', 1, 'ONY Healing 50% / Absorb 50% Nerf / 40% physical & 40% spell damage buff'),
--- (409, 0, '0.50', '0.50', '1.50', '1.50', 1, 'MC Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
+(409, 0, '0.50', '0.50', '1.50', '1.50', 0, 'MC Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
+-- TBC Raids
+(565, 0, '0.80', '0.80', '1.20', '1.20', 1, 'Normal Mode Gruul Healing 80% / Absorb 80% Nerf / 20% physical & 20% spell damage buff'),
+(565, 0, '0.50', '0.50', '1.50', '1.50', 64, 'Hard Mode Gruul Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
 -- Battlegrounds
 (30, 0, '0.80', '0.80', '0.70', '0.70', 1, 'AV Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
 (489, 0, '0.80', '0.80', '0.70', '0.70', 1, 'WSG Healing 20% / Absorb 20% / 30% physical & spell damage nerf'),
