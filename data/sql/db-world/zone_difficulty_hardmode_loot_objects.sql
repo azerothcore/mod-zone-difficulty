5996ec03b3d5772b84436586e064a12850ebaa06
55Honey <71938210+55Honey@users.noreply.github.com>
2023-01-29 23:39:46 +0100

    add magtheridon to hardmode encounters/maps

--- data/sql/db-world/zone_difficulty_hardmode_loot_objects.sql
+++ data/sql/db-world/zone_difficulty_hardmode_loot_objects.sql
@@ -2,10 +2,13 @@ DROP TABLE IF EXISTS `zone_difficulty_loot_objects`;
 CREATE TABLE `zone_difficulty_loot_objects` (
     `MapID` INT NOT NULL DEFAULT 0,
     `SourceEntry` INT NOT NULL,
-    `OverrideGO` INT NOT NULL DEFAULT 0,  -- 0 = no override
+    -- 0 = no override. Only required if the loot for an encounter is obtained
+    -- from e.g. a chest instead of a corpse.
+    `OverrideGO` INT NOT NULL DEFAULT 0,
 	PRIMARY KEY (`MapID`, `Entry`, `Type`)
 );
 
-INSERT INTO `zone_difficulty_loot_objects` (`MapID`, `Entry`, `Type`) VALUES
+INSERT INTO `zone_difficulty_loot_objects` (`MapID`, `SourceEntry`, `OverrideGO`) VALUES
+(544, 17257, 0),    -- Magtheridon, Magtheridon's Lair
 (565, 18831, 0),    -- High King Maulgar, Gruul's Lair
 (565, 19044, 0);    -- Gruul, Gruul's Lair
