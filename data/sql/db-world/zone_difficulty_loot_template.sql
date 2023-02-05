9354f4d51c7f6bbbb03d00848d83d4ead22d0c21
55Honey <71938210+55Honey@users.noreply.github.com>
2023-02-05 19:42:57 +0100

    add loot for hardmode

---
+++ data/sql/db-world/zone_difficulty_loot_template.sql
@@ -0,0 +1,6 @@
+DELETE FROM `creature_loot_template` WHERE `LootMode` = 64 AND `GroupId` = 64;
+
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(17257, 37, 0, 100, 0, 64, 64, 1, 1, 'Magtheridon - Hardmode'),
+(18831, 37, 0, 100, 0, 64, 64, 1, 1, 'High King Maulgar - Hardmode'),
+(19044, 37, 0, 100, 0, 64, 64, 1, 1, 'Gruul the Dragonkiller - Hardmode');
