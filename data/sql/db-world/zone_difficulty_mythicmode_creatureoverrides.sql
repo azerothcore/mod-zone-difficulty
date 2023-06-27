5e13fb96a37708ba258350dfc8e5362ec8dd9a3b
55Honey <71938210+55Honey@users.noreply.github.com>
2023-06-27 15:07:18 +0200

    Update zone_difficulty_mythicmode_creatureoverrides.sql

--- data/sql/db-world/zone_difficulty_mythicmode_creatureoverrides.sql
+++ data/sql/db-world/zone_difficulty_mythicmode_creatureoverrides.sql
@@ -12,4 +12,4 @@ INSERT INTO `zone_difficulty_mythicmode_creatureoverrides` (`CreatureEntry`, `HP
 (18831, 2.5, 1, 'Maulgar, Gruul\'s Lair HPx2.5'),
 (19044, 2.8, 1, 'Gruul, Gruul\'s Lair HPx2.8'),
 (17257, 2.5, 1, 'Magtheridon, Magtheridon\'s Lair HPx2.5'),
-(17881, 3.2, 1, 'Aeonus, Black Morass HPx3.2';
+(17881, 3.2, 1, 'Aeonus, Black Morass HPx3.2');
