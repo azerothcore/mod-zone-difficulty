d2ad25977778781f887c11953c54a1b69431588c
55Honey <71938210+55Honey@users.noreply.github.com>
2023-06-27 13:37:18 +0200

    COT mechanics

--- data/sql/db-world/zone_difficulty_mythicmode_creatureoverrides.sql
+++ data/sql/db-world/zone_difficulty_mythicmode_creatureoverrides.sql
@@ -11,4 +11,5 @@ INSERT INTO `zone_difficulty_mythicmode_creatureoverrides` (`CreatureEntry`, `HP
 (1128001, 1, 1, 'Chromie NPC - prevent changing hp.'),
 (18831, 2.5, 1, 'Maulgar, Gruul\'s Lair HPx2.5'),
 (19044, 2.8, 1, 'Gruul, Gruul\'s Lair HPx2.8'),
-(17257, 2.5, 1, 'Magtheridon, Magtheridon\'s Lair HPx2.5');
+(17257, 2.5, 1, 'Magtheridon, Magtheridon\'s Lair HPx2.5'),
+(17881, 3.2, 1, 'Aeonus, Black Morass HPx3.2';
