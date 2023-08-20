c0529e0c6777564fbfbb934229cf8411557241e3
55Honey <71938210+55Honey@users.noreply.github.com>
2023-08-20 22:10:53 +0200

    diasble AQ tuning

--- data/sql/db-world/zone_difficulty_info_content.sql
+++ data/sql/db-world/zone_difficulty_info_content.sql
@@ -1,8 +1,8 @@
 DELETE FROM `zone_difficulty_info`;
 INSERT INTO `zone_difficulty_info` (`MapId`, `PhaseMask`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
 -- Vanilla Raids
-(531, 0, '0.50', '0.50', '1.50', '1.30', 1, 'AQ40 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
-(509, 0, '0.50', '0.50', '1.60', '1.30', 1, 'AQ20 Healing 50% / Absorb 50% Nerf / 60% physical & 30% spell damage buff'),
+(531, 0, '0.50', '0.50', '1.50', '1.30', 0, 'AQ40 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
+(509, 0, '0.50', '0.50', '1.60', '1.30', 0, 'AQ20 Healing 50% / Absorb 50% Nerf / 60% physical & 30% spell damage buff'),
 (309, 0, '0.50', '0.50', '1.50', '1.10', 0, 'ZG20 Healing 50% / Absorb 50% Nerf / 50% physical & 10% spell damage buff'),
 (469, 0, '0.50', '0.50', '1.50', '1.50', 0, 'BWL Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
 (249, 0, '0.50', '0.50', '1.40', '1.40', 0, 'ONY Healing 50% / Absorb 50% Nerf / 40% physical & 40% spell damage buff'),
