ec54d781871200a14dda513093c8cf680737a0c7
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-09-19 17:38:18 -0300

    feat: add Black Temple instance data

---
+++ data/sql/db-world/zone_difficulty_mythicmode_bt_instance_data.sql
@@ -0,0 +1,12 @@
+DELETE FROM `zone_difficulty_mythicmode_instance_data` WHERE `MapID` = 564; -- Black Temple
+INSERT INTO `zone_difficulty_mythicmode_instance_data` (`MapID`, `SourceEntry`, `Override`, `InstanceType`) VALUES
+(564, 22887, 0, 11), -- Najentus
+(564, 22898, 0, 11), -- Supremus
+(564, 22841, 0, 11), -- Akama
+(564, 22871, 0, 11), -- Teron
+(564, 22947, 0, 11), -- Mother Shahraz
+(564, 22856, 0, 11), -- Reliquary
+(564, 22948, 0, 11), -- Gurtogg
+(564, 22951, 0, 11), -- Lady Malande
+(564, 22917, 0, 11); -- Illidan
+
