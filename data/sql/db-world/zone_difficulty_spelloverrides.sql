c7fad9c2916c956283060110d9d54717d1277c63
Dan <83884799+elthehablo@users.noreply.github.com>
2024-10-05 13:46:06 +0200

    Update zone_difficulty_spelloverrides.sql

--- data/sql/db-world/zone_difficulty_spelloverrides.sql
+++ data/sql/db-world/zone_difficulty_spelloverrides.sql
@@ -7,7 +7,7 @@ CREATE TABLE `zone_difficulty_spelloverrides` (
 	`Comment` TEXT
 );
 
-DELETE FROM `zone_difficulty_spelloverrides` WHERE `SpellID` IN (53652, 53654, 52752, 47753, 18562, 25516, 36240, 31850, 31851, 31852, 66235);
+DELETE FROM `zone_difficulty_spelloverrides` WHERE `SpellID` IN (53652, 53654, 52752, 47753, 18562, 25516, 36240, 31850, 31851, 31852, 66235, 39878, 42052, 40314, 40175, 40175, 41303, 41360, 40827, 40869, 40870, 40871, 41001, 42005, 41078, 41131);
 INSERT INTO `zone_difficulty_spelloverrides` (`SpellID`, `MapId`, `NerfValue`, `Enabled`, `Comment`) VALUES
 (53652, 0, 1, 1, 'Beacon of Light - Skip nerf.'),
 (53654, 0, 1, 1, 'Beacon of Light 2 (Flash of Light, Holy Shock) - Skip nerf.'),
@@ -20,4 +20,19 @@ INSERT INTO `zone_difficulty_spelloverrides` (`SpellID`, `MapId`, `NerfValue`, `
 (31850, 0, 1, 1, 'Ardent Defender - Absorb - Skip nerf'),
 (31851, 0, 1, 1, 'Ardent Defender - Absorb - Skip nerf'),
 (31852, 0, 1, 1, 'Ardent Defender - Absorb - Skip nerf'),
-(66235, 0, 1, 1, 'Ardent Defender - Heal - Skip nerf');
+(66235, 0, 1, 1, 'Ardent Defender - Heal - Skip nerf'),
+(39878, 0, 1, 1, 'Black Temple - Najentus - Tidal Burst Effect'),
+(42052, 0, 1, 1, 'Black Temple - Supremus - Volcanic Geyser'),
+(40314, 0, 1, 1, 'Black Temple - Vengeful Spirit - Spirit Volley'),
+(40175, 0, 1, 1, 'Black Temple - Vengeful Spirit - Spirit Chains'),
+(40157, 0, 1, 1, 'Black Temple - Vengeful Spirit - Spirit Lance'),
+(41303, 0, 1, 1, 'Black Temple - Reliquary of the Lost - Essence of Suffering - Soul Drain'),
+(41360, 0, 1, 1, 'Black Temple - Promenade Sentinel - Arcane charge'), -- may not work
+(40827, 0, 1, 1, 'Black Temple - Mother Shahraz - Sinful Beam'),
+(40869, 0, 1, 1, 'Black Temple - Mother Shahraz - Fatal Attraction'),
+(40870, 0, 1, 1, 'Black Temple - Mother Shahraz - Fatal Attraction'),
+(40871, 0, 1, 1, 'Black Temple - Mother Shahraz - Fatal Attraction'),
+(41001, 0, 1, 1, 'Black Temple - Mother Shahraz - Fatal Attraction'),
+(42005, 0, 1, 1, 'Black Temple - Gurtogg Bloodboil - Bloodboil'),
+(41078, 0, 1.1, 1, 'Black Temple - Illidan - Shadow Blast'),
+(41131, 0, 1.1, 1, 'Black Temple - Illidan - Flame Burst');
