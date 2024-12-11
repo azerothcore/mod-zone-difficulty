dba218d499b9d6128e11f632b0fd47e49a6048a1
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-12-11 00:17:06 -0300

    More items

--- data/sql/db-world/zone_difficulty_mythicmode_hyjal_rewards.sql
+++ data/sql/db-world/zone_difficulty_mythicmode_hyjal_rewards.sql
@@ -22,5 +22,15 @@ INSERT INTO `zone_difficulty_mythicmode_rewards` (`ContentType`, `ItemType`, `En
 (18, 3, 30879, @PRICE, 0, 0, 695, 1, 'Don Alejandro\'s Money Belt - Anetheron'),
 (18, 3, 30914, @PRICE, 0, 0, 695, 1, 'Belt of the Crescent Moon - Kazrogal'),
 (18, 3, 30863, @PRICE, 0, 0, 695, 1, 'Deadly Cuffs - Rage Winterchill'),
-(18, 3, 30868, @PRICE, 0, 0, 695, 1, 'Rejuvenating Bracers - Rage Winterchill');
+(18, 3, 30868, @PRICE, 0, 0, 695, 1, 'Rejuvenating Bracers - Rage Winterchill'),
 
+(18, 4, 30887, @PRICE, 0, 0, 695, 1, 'Golden Links of Restoration - Anetheron'),
+(18, 4, 30907, @PRICE2, 0, 0, 695, 1, 'Mail of Fevered Pursuit - Archimonde'),
+(18, 4, 30880, @PRICE, 0, 0, 695, 1, 'Quickstrider Moccasins - Anetheron'),
+(18, 4, 30873, @PRICE, 0, 0, 695, 1, 'Stillwater Boots - Rage Winterchill'),
+(18, 4, 30900, @PRICE, 0, 0, 695, 1, 'Bow-stitched Leggings - Azgalor'),
+(18, 4, 30893, @PRICE, 0, 0, 695, 1, 'Sun-touched Chain Leggings - Kaz\'rogal'),
+(18, 4, 30892, @PRICE, 0, 0, 695, 1, 'Beast-tamer\'s Shoulders - Kaz\'rogal'),
+(18, 4, 30919, @PRICE, 0, 0, 695, 1, 'Valestalker Girdle - Kaz\'rogal'),
+(18, 4, 30864, @PRICE, 0, 0, 695, 1, 'Bracers of the Pathfinder - Rage Winterchill'),
+(18, 4, 30869, @PRICE, 0, 0, 695, 1, 'Howling Wind Bracers - Rage Winterchill');
